0ad02874d9a8de46760d80e92ca1815c0e8231ce
chaoyun.xu <xuchaoyun@sioo.cn>
2020-05-22 14:31:00 +0800

    组件搭建

--- src/App.js
+++ src/App.js
@@ -2,42 +2,85 @@ import React from 'react';
 import './App.scss';
 import { Button, Input } from 'antd';
 
-class App extends React.Component {
-  state = {
-    val: '',
-    list: []
-  }
-  handleChange = (event) => {
-    let val = event.target.value
-    this.setState = ({
-      val
-    })
-  }
-  handleAdd = () => {
-    const { val, list } = this.state
-    list.push(val)
-    this.setState = ({
-      list
-    })
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       val: '',
+//       list: []
+//     }
+//   }
+//   handleChange = (event) => {
+//     let val = event.target.value
+//     this.setState = ({
+//       val
+//     })
 
+//   }
+//   handleAdd = () => {
+//     const { val, list } = this.state
+//     list.push(val)
+//     this.setState = ({
+//       list
+//     })
+
+//   }
+//   render() {
+//     const { val, list } = this.state
+//     const LiItem = list.map((item) => {
+//       return <li>{item}</li>
+//     })
+
+//     return (< div  >
+//       <h1>Hello WOrld</h1>
+//       <Input type="text" style={{ width: 300 }} onChange={this.handleChange} />
+//       <Input type="text" value={val} style={{ width: 300 }} onChange={this.handleChange}></Input>
+//       <Button type="primary" onClick={this.handleAdd}>添加</Button>
+//       <ul>
+//         {LiItem}
+//       </ul>
+//     </div >)
+//   }
+// }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: '', list: [] };
+
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleAdd = this.handleAdd.bind(this);
   }
+
+  handleChange = (event) => {
+    this.setState({ value: event.target.value });
+  }
+
+  handleAdd = () => {
+    var { value, list } = this.state
+    list.push(value)
+    console.log(list)
+    this.setState({ list })
+  }
+
   render() {
-    const { val, list } = this.state
-    const LiItem = list.map((item) => {
-      return <li>{item}</li>
-    })
     return (
       <div className="App">
-        <h1>Hello WOrld</h1>
-        <Input type="text" value={val} style={{ width: 300 }} onChange={this.handleChange}></Input>
+        <h1>Hello World</h1>
+        <label>
+          <Input type="text" style={{ width: 300 }} value={this.state.value} onChange={this.handleChange} />
+        </label>
         <Button type="primary" onClick={this.handleAdd}>添加</Button>
         <ul>
-          {LiItem}
+          <li>
+            {this.state.list.map((item) => {
+              return <li>{item}</li>
+            })}
+          </li>
         </ul>
-      </div >)
+      </div>
+    );
   }
 }
-
 // function App() {
 //   return (
 //     <div className="App">
